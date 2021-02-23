a8987fceba40184bf450f2dd71e056b74214100a
rashad <gasimov.r.rg@gmail.com>
2021-02-23 19:51:44 +0400

    initial commit

---
+++ chat/urls.py
@@ -0,0 +1,9 @@
+# chat/urls.py
+from django.urls import path
+
+from .views import index, room
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+]
