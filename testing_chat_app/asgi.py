81f51d7779cf0a001ee72a9de911219ad9924e56
rashad <gasimov.r.rg@gmail.com>
2021-03-12 00:11:28 +0400

    nc

--- testing_chat_app/asgi.py
+++ testing_chat_app/asgi.py
@@ -12,8 +12,6 @@ django_asgi_app = get_asgi_application()
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from chat.consumers import AdminChatConsumer, PublicChatConsumer
-
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
     "http": django_asgi_app,
@@ -21,8 +19,7 @@ application = ProtocolTypeRouter({
     # WebSocket chat handler
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            url(r"^chat/admin/$", AdminChatConsumer.as_asgi()),
-            url(r"^chat/$", PublicChatConsumer.as_asgi()),
+            chat.routing.websocket_urlpatterns
         ])
     ),
 })
