3db7fa8bed827decd9a38e7e0d9ed2a013f1f28b
rashad <gasimov.r.rg@gmail.com>
2021-03-12 00:07:43 +0400

    nc

--- testing_chat_app/asgi.py
+++ testing_chat_app/asgi.py
@@ -1,10 +1,28 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_chat_app.settings')
-django.setup()
-application = get_default_application()
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testing_chat_app.settings")
+django_asgi_app = get_asgi_application()
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.consumers import AdminChatConsumer, PublicChatConsumer
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/admin/$", AdminChatConsumer.as_asgi()),
+            url(r"^chat/$", PublicChatConsumer.as_asgi()),
+        ])
+    ),
+})
