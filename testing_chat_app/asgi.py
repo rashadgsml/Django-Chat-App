b077ded326e27904f4daf22f4601ed9148a42217
rashad <gasimov.r.rg@gmail.com>
2021-03-11 23:56:41 +0400

    nc

--- testing_chat_app/asgi.py
+++ testing_chat_app/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from django.core.asgi import get_asgi_application
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_chat_app.settings')
 django.setup()
