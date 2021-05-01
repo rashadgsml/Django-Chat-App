3f0ce7d1e8b95b2447a06ab382b4731bc04879a5
rashad <gasimov.r.rg@gmail.com>
2021-05-01 22:46:27 +0400

    nc

--- testing_chat_app/settings.py
+++ testing_chat_app/settings.py
@@ -108,12 +108,7 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
